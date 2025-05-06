d9fc73fdb9a4f1dcf57bdb3a0ee672d17e5186f7
Clarence Panto <clardaing128@gmail.com>
2025-05-05 23:00:49 -0700

    created table for recipe-products-ingredients

--- migrations/20250506024744_products.js
+++ migrations/20250506024744_products.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.integer("price").notNullable();
+    table.string("image_url").notNullable();
+    table.boolean("availability").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("products");
+}
