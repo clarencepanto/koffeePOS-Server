68730800909886773d23cba4ab15c4d1ed41c11b
Clarence Panto <clardaing128@gmail.com>
2025-05-08 23:54:19 -0700

    added post request to sales and ingredients

--- routes/ingredients-routes.js
+++ routes/ingredients-routes.js
@@ -4,6 +4,17 @@ const knex = initKnex(configuration);
 import express from "express";
 const router = express.Router();
 
+// middleware
+const checkpoint = (req, res, next) => {
+  const { name, stock, unit } = req.body;
+  if (!name || !stock || !unit) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all required information" });
+  }
+  next();
+};
+
 // fetch all ingredient order data
 router.get("/", async (req, res) => {
   try {
@@ -14,6 +25,23 @@ router.get("/", async (req, res) => {
       message: `Unable to retrieve ingredient data`,
     });
     console.error("Error fetching ingredientData", error);
+  }
+});
+
+// create new ingredient
+router.post("/", checkpoint, async (req, res) => {
+  try {
+    const [id] = await knex("ingredients").insert(req.body);
+
+    const newIngredient = await knex("ingredients")
+      .select(["id", "name", "stock", "unit"])
+      .where({ id })
+      .first();
+    res.status(201).json(newIngredient);
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ message: "No new ingredient generated. Please try again" });
   }
 });
 
--- routes/sales-routes.js
+++ routes/sales-routes.js
@@ -4,6 +4,17 @@ const knex = initKnex(configuration);
 import express from "express";
 const router = express.Router();
 
+// middleware
+const checkpoint = (req, res, next) => {
+  const { product_id, quantity, unit_price, total_price } = req.body;
+  if (!product_id || !quantity || !unit_price || !total_price) {
+    return res
+      .status(400)
+      .json({ message: "Please fill all required information" });
+  }
+  next();
+};
+
 // fetch all ingredient order data
 router.get("/", async (req, res) => {
   try {
@@ -14,6 +25,23 @@ router.get("/", async (req, res) => {
       message: `Unable to retrieve sales data`,
     });
     console.error("Error fetching salesData", error);
+  }
+});
+
+// create new ingredient
+router.post("/", checkpoint, async (req, res) => {
+  try {
+    const [id] = await knex("sales").insert(req.body);
+
+    const newIngredient = await knex("sales")
+      .select(["product_id", "quantity", "unit_price", "total_price"])
+      .where({ id })
+      .first();
+    res.status(201).json(newIngredient);
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ message: "No new sales generated. Please try again" });
   }
 });
 
